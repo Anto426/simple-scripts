e8e6a27517171144548a67a6d585a13f92d8fd71
Anto426 <amanocchio426@gmail.com>
2022-10-22 17:28:29 +0200

    update

---
+++ organizzatore di file/general.bat
@@ -0,0 +1,38 @@
+@echo off 
+
+echo welcome to this script 
+
+
+set /A  years = %DATE:~6,4%
+
+
+set /A newyears = years + 1 
+
+set backupFilename=A%DATE:~6,4%-%newyears%
+
+
+set list_1=Italiano Storia Inglese Informatica Matematica TP TLC 
+set list_2 = Informatica TP TLC 
+echo %backupFilename%
+:loop 
+IF !cout! lss !limit! (
+      set /A cout = !cout! + 1 
+    IF EXIST %backupFilename%\ (
+      cd %backupFilename%\
+          (for %%p in (%list_1%) do (
+               echo  cartelle create %%p
+               mkdir %%p
+           )) 
+
+    ) ELSE (
+        echo %backupFilename% missing.
+        mkdir %backupFilename%
+        goto :loop
+    )
+)
+
+  (for %%p in (%list_2%) do (
+    cd %%p      
+    mkdir %%p
+   )) 
+
